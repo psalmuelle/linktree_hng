c0d8662a994df391c26a0dd07dc6162b15c48a9a
psalmuelle <bjoestores@gmail.com>
2022-10-24 23:30:18 +0100

    create header

--- linktree/src/App.js
+++ linktree/src/App.js
@@ -1,9 +1,26 @@
-
+import React, {useEffect} from 'react'
 import './App.css';
 
 function App() {
+  useEffect(()=>{
+
+  })
   return (
     <div className="App">
+
+      <header>
+      <div></div>
+      </header>
+
+
+      <main>
+
+      </main>
+
+
+      <footer>
+
+      </footer>
       
     </div>
   );
