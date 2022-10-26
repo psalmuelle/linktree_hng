3a4f663c219043072218271fd884e53c02302a87
psalmuelle <bjoestores@gmail.com>
2022-10-26 15:38:37 +0100

    finish styling

--- linktree/src/App.js
+++ linktree/src/App.js
@@ -12,18 +12,18 @@ const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/
 
 function App() {
   useEffect(()=>{
+document.title = "hng_task"
 
   })
   return (
     <div className="App">
 
       <header>
-      <div id='profile_id--container'>
-        <img id='profile_id' src={images["me.jpeg"]} alt="Erinle Samuel"/>
-      </div>
-      <h1>Erinle Samuel</h1>
+        <img id='profile__img' src={images["me.jpeg"]} alt="Erinle Samuel"/>
+      <h1 id='twitter'>Erinle Samuel</h1>
+      <h2 id='slack' hidden>erinle_sam</h2>
 
-      <div>
+      <div id='share__container' title='Share Link'>
         <img src={images["share_Icon.svg"]} alt="Share"/>{/* Share icons changes to 3dots on smaller screen */}
       </div>
       
@@ -31,35 +31,32 @@ function App() {
 
 
       <main>
-    <div className='link_container'>
-      <a id='twitter' href='#' target="_blank">Twitter Link</a>
-    </div>
-
-    <div className='link_container' hidden >
-      <a id='slack' href='#' target="_blank">Slack Link</a>
-    </div>   
-
-    <button id='btn_zuri' className='link_btn'>
-      <a href='https://training.zuri.team/' target="_blank">Zuri Team</a>
+    
+    <button id='twitter__link' className='link__btn'>
+      <a  href='https://twitter.com/erinle_sam' target="_blank" rel='noreferrer'>Twitter Link</a>
     </button>
 
-    <button id='books' className='link_btn'>
-      <a href='http://books.zuri.team/' target="_blank">Zuri Books</a>
+    <button id='btn__zuri' className='link__btn'>
+      <a href='https://training.zuri.team/' target="_blank" rel='noreferrer'>Zuri Team</a>
     </button>
 
-    <button id='book_python' className='link_btn'>
-      <a href='https://books.zuri.team/python-for-beginners?ref_id=erinle_sam' target="_blank">Python Books</a>
+    <button id='books' className='link__btn'>
+      <a href='http://books.zuri.team/' target="_blank" rel='noreferrer'>Zuri Books</a>
     </button>
 
-    <button id='pitch' className='link_btn'>
-      <a href='https://background.zuri.team/' target="_blank">Checkout Top-notch Coders</a>
+    <button id='book__python' className='link__btn'>
+      <a href='https://books.zuri.team/python-for-beginners?ref_id=erinle_sam' target="_blank" rel='noreferrer'>Python Books</a>
     </button>
 
-    <button id='book_design' className='link_btn'>
-      <a href='https://books.zuri.team/design-rules' target="_blank">Design Books</a>
+    <button id='pitch' className='link__btn'>
+      <a href='https://background.zuri.team/' target="_blank" rel='noreferrer'>Checkout Top-notch Coders</a>
     </button>
 
-    <figure>
+    <button id='book__design' className='link__btn'>
+      <a href='https://books.zuri.team/design-rules' target="_blank" rel='noreferrer'>Design Books</a>
+    </button>
+
+    <figure id='social__icons'>
       <img src={images["slack_icon.svg"]} alt="slack"/>
       <img src={images["github_icon.svg"]} alt="github"/>
     </figure>
@@ -71,7 +68,7 @@ function App() {
       <footer>
      <div>
       <img src={images["zuri_logo.svg"]} alt="zuri logo"/>
-      <p>HNG Internship 9 Frontend Task</p>
+      <div id='hng__task'>HNG Internship 9 Frontend Task</div>
       <img src={images["I4G_logo.svg"]} alt="I4G logo"/>
      </div>
       </footer>
